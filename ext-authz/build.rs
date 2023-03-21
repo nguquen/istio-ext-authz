3d9d68a89d2af16736cbe9b916d73a007f170562
thienlxp <thien.le@leap.expert>
2023-03-21 08:45:43 +0700

    impl ExtAuth

--- ext-authz/build.rs
+++ ext-authz/build.rs
@@ -1,4 +1,16 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
-    Ok(())
+fn main() {
+    tonic_build::configure()
+        .build_server(true)
+        .build_client(false)
+        .include_file("mod.rs")
+        .compile(
+            &["proto/envoy/api/envoy/service/auth/v3/external_auth.proto"],
+            &[
+                "proto/envoy/api",
+                "proto/protoc-gen-validate",
+                "proto/googleapis",
+                "proto/udpa",
+            ],
+        )
+        .unwrap();
 }
--- ext-authz/src/main.rs
+++ ext-authz/src/main.rs
@@ -1,41 +1,97 @@
-use tonic::{transport::Server, Request, Response, Status};
+use service_auth::{
+    envoy::{
+        r#type::v3::{HttpStatus, StatusCode},
+        service::auth::v3::{
+            authorization_server::Authorization, authorization_server::AuthorizationServer,
+            check_response::HttpResponse, CheckRequest, CheckResponse, DeniedHttpResponse,
+            OkHttpResponse,
+        },
+    },
+    google::rpc::Status,
+};
+use tonic::{transport::Server, Request, Response};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
-
-pub mod hello_world {
-    tonic::include_proto!("helloworld"); // The string specified here must match the proto package name
+pub mod service_auth {
+    tonic::include_proto!("mod");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct ExtAuth {}
 
-#[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
-        &self,
-        request: Request<HelloRequest>, // Accept request of type HelloRequest
-    ) -> Result<Response<HelloReply>, Status> {
-        // Return an instance of type HelloReply
-        println!("Got a request: {:?}", request);
-
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
+impl ExtAuth {
+    fn deny(&self) -> Result<Response<CheckResponse>, tonic::Status> {
+        let resp = CheckResponse {
+            status: Some(Status {
+                code: StatusCode::Forbidden as i32,
+                message: String::from("Forbidden"),
+                details: vec![],
+            }),
+            dynamic_metadata: None,
+            http_response: Some(HttpResponse::DeniedResponse(DeniedHttpResponse {
+                status: Some(HttpStatus {
+                    code: StatusCode::Forbidden as i32,
+                }),
+                headers: vec![],
+                body: String::from("Forbidden access"),
+            })),
         };
 
-        Ok(Response::new(reply)) // Send back our formatted greeting
+        Ok(Response::new(resp))
+    }
+
+    fn allow(&self) -> Result<Response<CheckResponse>, tonic::Status> {
+        let resp = CheckResponse {
+            status: Some(Status {
+                code: StatusCode::Ok as i32,
+                message: String::from("Ok"),
+                details: vec![],
+            }),
+            dynamic_metadata: None,
+            http_response: Some(HttpResponse::OkResponse(OkHttpResponse {
+                headers: vec![],
+                headers_to_remove: vec![],
+                dynamic_metadata: None,
+                response_headers_to_add: vec![],
+                query_parameters_to_set: vec![],
+                query_parameters_to_remove: vec![],
+            })),
+        };
+
+        Ok(Response::new(resp))
+    }
+}
+
+#[tonic::async_trait]
+impl Authorization for ExtAuth {
+    async fn check(
+        &self,
+        request: Request<CheckRequest>,
+    ) -> Result<Response<CheckResponse>, tonic::Status> {
+        let token = request
+            .into_inner()
+            .attributes
+            .and_then(|a| a.request)
+            .and_then(|r| r.http)
+            .map(|h| h.headers)
+            .and_then(|headers| headers.get("token").cloned());
+
+        if token == Some(String::from("secured")) {
+            return self.allow();
+        }
+
+        return self.deny();
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::]:5005".parse()?;
-    let greeter = MyGreeter::default();
+    let ext_auth = ExtAuth::default();
 
     println!("Server started at :5005");
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(AuthorizationServer::new(ext_auth))
         .serve(addr)
         .await?;
 
